660728f609b1ff880fb7f7aa8e77a84f6705a0a3
Ukasyah <hi.ukasyah@gmail.com>
2025-01-25 17:33:17 +0700

    add shutdown and validator

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
 pub mod error;
+pub mod shutdown;
+pub mod validator;
---
+++ src/shutdown.rs
@@ -0,0 +1,12 @@
+use signal_hook::{
+    consts::{SIGINT, SIGTERM},
+    iterator::Signals,
+};
+
+pub async fn wait_for_signal() {
+    let mut signals = Signals::new([SIGINT, SIGTERM]).unwrap();
+
+    if signals.forever().next().is_some() {
+        log::info!("received signal, shutting down");
+    }
+}
